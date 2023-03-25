47b0fa67f6a4f3b28bcaff26e47be2cc7eeebb36
Zdeněk Odehnal <odehnal.zdenek@vinisto.cz>
2023-03-25 17:40:03 +0100

    Added bag of words

--- main.rs
+++ main.rs
@@ -1,4 +1,7 @@
-use std::{error::Error, fs::File, io::Read};
+use rayon::prelude::*;
+use std::io::Write;
+use std::sync::{Arc, Mutex};
+use std::{collections::HashMap, error::Error, fs::File, io::Read};
 
 use crate::structs::PreprocessedReview;
 
@@ -9,23 +12,93 @@ extern crate csv;
 extern crate serde;
 
 fn main() {
-    print_csv().unwrap();
+    let num_rows = 20000;
+    let (dictionary, review_vectors) = bag_of_words(num_rows);
+
+    let mut mapped_dictionary =
+        map_dictionary_to_review_vectors(&dictionary, &review_vectors, None);
+
+    // Sort the mapped_dictionary by count in descending order
+    mapped_dictionary.sort_by(|a, b| b.1.cmp(&a.1));
+
+    // Write the sorted mapped_dictionary to a file
+    let file_path = "bag_of_words.csv";
+    let mut file = std::fs::File::create(file_path).expect("Unable to create file");
+
+    // Write the column labels
+    writeln!(&mut file, "weight,word").expect("Unable to write column labels");
+
+    // Write the data
+    for (word, count) in mapped_dictionary.iter() {
+        writeln!(&mut file, "{},{}", count, word).expect("Unable to write data");
+    }
+
+    println!("Bag of words data saved to {}", file_path);
 }
 
-fn print_csv() -> Result<(), Box<dyn Error>> {
+fn bag_of_words(num_rows: usize) -> (Vec<String>, Vec<Vec<u32>>) {
     let file_path = "preprocessed_data.csv";
+    let preprocessed_reviews = load_csv(file_path).unwrap();
 
-    let preprocessed_reviews = load_csv(file_path)?;
+    let word_counts: Arc<Mutex<HashMap<String, u32>>> = Arc::new(Mutex::new(HashMap::new()));
 
-    for (i, review) in preprocessed_reviews.iter().enumerate() {
-        if i % 100 == 0 {
-            println!("{:?}", review);
-            println!();
-            std::thread::sleep(std::time::Duration::from_millis(100));
+    preprocessed_reviews
+        .par_iter()
+        .take(num_rows)
+        .enumerate()
+        .for_each(|(_, review)| {
+            let mut local_word_counts = HashMap::new();
+            let words = review.review_text.split_whitespace();
+            for word in words {
+                *local_word_counts.entry(word.to_string()).or_insert(0) += 1;
+            }
+
+            let mut word_counts = word_counts.lock().unwrap();
+            for (word, count) in local_word_counts {
+                *word_counts.entry(word).or_insert(0) += count;
+            }
+        });
+
+    let dictionary: Vec<String> = word_counts.lock().unwrap().keys().cloned().collect();
+
+    let review_vectors: Vec<Vec<u32>> = preprocessed_reviews
+        .par_iter()
+        .take(num_rows)
+        .enumerate()
+        .map(|(_, review)| {
+            let mut review_vector: Vec<u32> = vec![0; dictionary.len()];
+            let words = review.review_text.split_whitespace();
+            for word in words {
+                if let Some(index) = dictionary.iter().position(|w| w == word) {
+                    review_vector[index] += 1;
+                }
+            }
+            review_vector
+        })
+        .collect();
+
+    (dictionary, review_vectors)
+}
+
+fn map_dictionary_to_review_vectors(
+    dictionary: &[String],
+    review_vectors: &[Vec<u32>],
+    num_reviews_to_process: Option<usize>,
+) -> Vec<(String, u32)> {
+    let num_reviews = num_reviews_to_process.unwrap_or_else(|| review_vectors.len());
+    let mut combined_counts: Vec<u32> = vec![0; dictionary.len()];
+
+    for review_vector in review_vectors.iter().take(num_reviews) {
+        for (j, count) in review_vector.iter().enumerate() {
+            combined_counts[j] += count;
         }
     }
 
-    Ok(())
+    dictionary
+        .iter()
+        .cloned()
+        .zip(combined_counts.into_iter())
+        .collect()
 }
 
 fn load_csv(file_path: &str) -> Result<Vec<PreprocessedReview>, Box<dyn Error>> {
